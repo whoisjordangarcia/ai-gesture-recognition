184815c9c064c5bf92ddcb44bbc5a4c3d5bee800
Jordan Garcia <arickho@gmail.com>
2022-02-12 13:00:51 -0500

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,25 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-import React, { CanvasHTMLAttributes, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "@tensorflow/tfjs-backend-webgl";
 
 import ts from "@tensorflow-models/tfjs";
 import { load } from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import { drawHands } from "../utils/handPoints";
+import fp from "fingerpose";
 
 const GestureRecognition: NextPage = () => {
   const webcamRef = useRef<Webcam | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [emoji, setEmoji] = useState(null);
+
+  const images = {
+    thumbs_up: "/thumbs_up.png",
+    victory: "/victory.png",
+  };
 
   const runHandpose = async () => {
     const net = await load();
@@ -22,7 +30,7 @@ const GestureRecognition: NextPage = () => {
 
     setInterval(() => {
       detect(net);
-    }, 1);
+    }, 100);
   };
 
   const detect = async (net: any) => {
@@ -31,16 +39,35 @@ const GestureRecognition: NextPage = () => {
       canvasRef?.current !== null
     ) {
       const video = webcamRef.current.video;
-      webcamRef.current.video.height = video.videoHeight;
-      webcamRef.current.video.width = video.videoWidth;
 
       canvasRef.current.width = video.videoWidth;
       canvasRef.current.height = video.videoHeight;
 
       const hand = await net.estimateHands(video);
 
-      console.log(hand);
+      // console.log(hand);
 
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+
+        const gesture = await GE.estimate(hand[0].landmarks, 8);
+        // console.log(gesture.gestures);
+
+        if (gesture?.gestures.length > 0) {
+          const confidenceScore = gesture.gestures?.map(
+            (prediction: any) => prediction.score
+          );
+
+          const maxConfidence = confidenceScore?.indexOf(
+            Math.max.apply(null, confidenceScore)
+          );
+
+          setEmoji(gesture.gestures[maxConfidence].name);
+        }
+      }
       const ctx = canvasRef.current.getContext("2d");
 
       if (ctx) {
@@ -62,6 +89,7 @@ const GestureRecognition: NextPage = () => {
       <main className={styles.main}>
         <Webcam ref={webcamRef} className={styles.webcam}></Webcam>
         <canvas ref={canvasRef} className={styles.webcam} />
+        {emoji !== null && <img className={styles.emoji} src={images[emoji]} />}
       </main>
     </div>
   );
